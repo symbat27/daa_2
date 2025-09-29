fe91f3d389829ff3d8f6d2c835c5224278c35422
symbat27 <242262@astanait.edu.kz>
2025-09-29 16:49:02 +0500

    SelectionSort.java

--- src/main/java/algorithms/SelectionSort.java
+++ src/main/java/algorithms/SelectionSort.java
@@ -1,4 +1,31 @@
 package algorithms;
 
+import metrics.PerformanceTracker;
+
 public class SelectionSort {
+    public static void sort(int[] arr, PerformanceTracker pt) {
+        if (arr == null) throw new IllegalArgumentException("Array is null");
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            pt.addAccess();
+
+            for (int j = i + 1; j < n; j++) {
+                pt.addAccess();
+                pt.addComparison();
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+
+            if (minIdx != i) {
+                int tmp = arr[i];
+                arr[i] = arr[minIdx];
+                arr[minIdx] = tmp;
+                pt.addSwap();
+                pt.addAccesses(3);
+            }
+        }
+    }
 }
