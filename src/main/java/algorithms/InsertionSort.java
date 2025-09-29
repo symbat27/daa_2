57510e5062ec30a6e0c8829d66dac50d2d36ec58
symbat27 <242262@astanait.edu.kz>
2025-09-29 16:47:58 +0500

    InsertionSort.java

--- src/main/java/algorithms/InsertionSort.java
+++ src/main/java/algorithms/InsertionSort.java
@@ -1,4 +1,33 @@
 package algorithms;
 
+import metrics.PerformanceTracker;
+
 public class InsertionSort {
+    public static void sort(int[] arr, PerformanceTracker pt) {
+        if (arr == null) throw new IllegalArgumentException("Array is null");
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i]; pt.addAccesses(1);
+            int left = 0, right = i - 1;
+
+            while (left <= right) {
+                int mid = (left + right) >>> 1;
+                pt.addAccesses(1); pt.addComparison();
+                if (key < arr[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            int j = i - 1;
+            while (j >= left) {
+                arr[j + 1] = arr[j];
+                pt.addAccesses(2); // read+write
+                pt.addSwap();
+                j--;
+            }
+            arr[left] = key; pt.addAccesses(1);
+        }
+    }
 }
